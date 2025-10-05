4696461b0c1a64ea5b88dba3789e172631f2ace7
Leighton Grant <oneal.grant@outlook.com>
2025-10-05 12:57:19 +0100

    feat: Improve project card layout and styling
    - Wrap project cards in columns for a responsive grid layout.
    - Ensure consistent card height for a cleaner look.
    - Use flexbox on the card footer to space out links.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,13 +2,14 @@ import { projects } from './data.js'
 $(document).ready(() => {
 	const projectContent = $('.project-content')
 	$.each(projects, (_, project) => {
-		const card = $('<div class="card" style="width: 18rem"></div>')
+		const col = $('<div class="col"></div>')
+		const card = $('<div class="card h-100"></div>')
 		const imageDiv = $('<div></div>')
 		const image = $('<img class="card-img-top"/>')
 		const cardBody = $('<div class="card-body"></div>')
 		const cardTitle = $('<h5 class="card-title fs-3 fw-bold"></h5>')
 		const cardText = $('<p class="card-text"></p>')
-		const cardFooter = $('<div class="card-footer"></div>')
+		const cardFooter = $('<div class="card-footer d-flex justify-content-between align-items-center text-uppercase fw-semibold"></div>')
 		const repoLink = $('<a class="repo-link" target="_blank" rel="noopener">Repo &#129125;</a>')
 		const projectLink = $('<a class="project-link" target="_blank" rel="noopener">Demo &#129125;</a>')
 
@@ -22,7 +23,8 @@ $(document).ready(() => {
 		cardBody.append(cardTitle, cardText)
 		cardFooter.append(repoLink, projectLink)
 		card.append(imageDiv, cardBody, cardFooter)
+		col.append(card)
 
-		projectContent.append(card)
+		projectContent.append(col)
 	})
 })
